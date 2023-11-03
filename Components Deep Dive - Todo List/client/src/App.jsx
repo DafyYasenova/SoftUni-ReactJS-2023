f1f272f9d19251442d9c4c921eb25f6a0f838070
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-03 10:15:16 +0200

    add footer and todo list components

--- Components Deep Dive - Todo List/client/src/App.jsx
+++ Components Deep Dive - Todo List/client/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 
 import './App.css'
 import Loading from './components/Loading'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TodoList from './components/TodoList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +12,7 @@ function App() {
   return (
     <>
      
- 
+ <Header />
 
  
   <main classNameName="main">
@@ -28,106 +31,13 @@ function App() {
        {/* <Loading /> */}
 
       
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-header-task">Task</th>
-              <th className="table-header-status">Status</th>
-              <th className="table-header-action">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-
-          
-            <tr className="todo is-completed">
-              <td>Give dog a bath</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-          
-            <tr className="todo is-completed">
-              <td>Do laundry</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-           
-            <tr className="todo">
-              <td>Vacuum floor</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            
-            <tr className="todo is-completed">
-              <td>Feed cat</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-           
-            <tr className="todo">
-              <td>Change light bulbs</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            
-            <tr className="todo is-completed">
-              <td>Feed cat</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-           
-            <tr className="todo">
-              <td>Change light bulbs</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-          
-            <tr className="todo is-completed">
-              <td>Go to Store</td>
-              <td>Completed</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-           
-            <tr classNameName="todo">
-              <td>Fill gas tank</td>
-              <td>Incomplete</td>
-              <td classNameName="todo-action">
-                <button classNameName="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <TodoList />
       </div>
     </section>
   </main>
 
-  
-  <footer classNameName="footer">
-    <p>Copyright © designed by Mihail Valkov</p>
-  </footer>
+  <Footer />
+ 
     </>
   )
 }
