e37e33513959cd6abf418132981edd3523a7034b
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-16 07:29:39 +0200

    modify styles on login component

--- Games Play/client/src/components/Login/Login.jsx
+++ Games Play/client/src/components/Login/Login.jsx
@@ -4,19 +4,19 @@ import styles from './Login.module.css';
 export default function Login(){
     return(
         <>
-        <section id="login-page" className="auth" > 
-        <form id="login" >
+        <section id={styles["login-page"]} className={styles.auth} > 
+        <form id={styles.login} >
 
-            <div className="container" >
-                <div className="brand-logo"></div>
+            <div className={styles.container} >
+                <div className={styles["brand-logo"]}></div>
                 <h1>Login</h1>
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
 
                 <label htmlFor="login-pass">Password:</label>
                 <input type="password" id="login-password" name="password" />
-                <input type="submit" className="btn submit" value="Login" />
-                <p className="field">
+                <input type="submit" className={styles.submit} value="Login" />
+                <p className={styles.field}>
                     <span>If you don't have profile click <a href="#">here</a></span>
                 </p>
             </div>
