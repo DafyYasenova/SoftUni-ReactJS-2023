d0e20fbaf015f2660379656eb9ef72ccd6897f5e
Dafy Yasenova <dafy_yasenova@abv.bg>
2023-11-16 16:47:54 +0200

    add request

--- Games Play/client/src/services/gameService.js
+++ Games Play/client/src/services/gameService.js
@@ -1,24 +1,19 @@
-import { request} from '../lib/request';
+import * as request from '../lib/request';
 
 const baseUrl = 'http://localhost:3030/jsonstore/catalog';
 
 
 export const create = async ({ title, category, maxLevel, imageUrl, summary }) => {
-    const response = await fetch(`${baseUrl}`, {
+    const result = await request.post(baseUrl, { title, maxLevel, category, imageUrl, summary });
 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ title, maxLevel, category, imageUrl, summary })
-    });
 
-    const result = await response.json();
     return result;
 
 }
 
 export const getAll = async () => {
-const result = await request ('GET', baseUrl);
-return Object.values(result);
+
+    const result = await request.get(baseUrl);
+
+    return Object.values(result);
 }
